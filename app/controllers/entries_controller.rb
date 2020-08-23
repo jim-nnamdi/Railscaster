1f7c52d7faf32e392a75f6f0f035100e807a830d
jim-nnamdi <jimsamuel50@gmail.com>
2020-08-24 00:29:41 +0100

    Update/Edit controller function updated

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -19,9 +19,32 @@ class EntriesController < ApplicationController
     def create 
         @entry = entry.new(entries_parameters)
         if @entry.save 
-            redirect_to :action => 'new'
+            redirect_to :action => 'index'
         else
-            render :action => 'index'
+            render :action => 'new'
         end
     end
+
+    # edit an entry 
+    def edit 
+        @entry = entry.find(params[:id])
+    end
+
+    # update an entry 
+    def update 
+        @entry = entry.find(params[:id])
+        if @entry.update_attributes(entries_parameters)
+            redirect_to :action => 'show', :id => @entry
+        else 
+            render :action => 'edit'
+        end
+    end
+
+   # custom function that holds parameters to
+   # be passed when making an entry.
+
+   def entries_parameters 
+    params.require(:entry).permit(:title, :description, :author)
+   end
+
 end
