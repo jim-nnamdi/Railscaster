d08b27c9e76846fe756a8fa750451d929afb94c0
jim-nnamdi <jimsamuel50@gmail.com>
2020-08-24 00:14:22 +0100

    added columns to migration files

--- db/migrate/20200823230734_create_entries.rb
+++ db/migrate/20200823230734_create_entries.rb
@@ -2,6 +2,10 @@ class CreateEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :entries do |t|
 
+      t.column :title, :string, :limit = 32,  :null=false
+      t.column :description , :text, :null=false 
+      t.column :author
+      t.column :created_at, :timestamp
       t.timestamps
     end
   end
