2b61c673492c6152384499d1d99eb9b7ed2b8142
jim-nnamdi <jimsamuel50@gmail.com>
2020-08-24 00:18:10 +0100

    Fixed errors on migration files

--- db/migrate/20200823230734_create_entries.rb
+++ db/migrate/20200823230734_create_entries.rb
@@ -2,10 +2,9 @@ class CreateEntries < ActiveRecord::Migration[6.0]
   def change
     create_table :entries do |t|
 
-      t.column :title, :string, :limit = 32,  :null=false
-      t.column :description , :text, :null=false 
-      t.column :author
-      t.column :created_at, :timestamp
+      t.column :title, :string, :limit => 32,  :null=>false
+      t.column :description , :text
+      t.column :author, :string
       t.timestamps
     end
   end
