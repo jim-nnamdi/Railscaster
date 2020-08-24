6d4e18e2bdf96272702ce08a166ac591c531cd67
jim-nnamdi <jimsamuel50@gmail.com>
2020-08-24 01:25:10 +0100

    Update routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
   # in setting the root route { root :to => "controller#action" }
-  
+
   root :to => "entries#index"
+  get 'entries/new'
+  get 'entries/show'
+  post 'entries/create'
+  patch 'entries/update'
+  get 'entries/edit'
+  get 'entries/delete'
+  get 'entries/update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
