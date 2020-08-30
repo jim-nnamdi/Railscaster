ee8359d5b7f7f0c366bb03e92f02a84ddc67a1c4
jim-nnamdi <jimsamuel50@gmail.com>
2020-08-31 00:49:01 +0100

    Initialized active storage and also set relationship for entry Image

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,4 +2,7 @@ class Entry < ApplicationRecord
     validates_presence_of :title 
     validates_presence_of :description
     validates_presence_of :author
+
+    # addition of image
+    has_one_attached :entryimage
 end
